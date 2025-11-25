70e764da06b3d36605e33bc862bb901ebbc8f28c
nishanthinge3700-lang <nishant.hinge3700@gmail.com>
2025-11-25 23:10:09 +0530

    Owner Change and Bot name Change

--- config.ts
+++ config.ts
@@ -30,8 +30,8 @@ function getDateAndTime(): string {
 
 export const DATE_AND_TIME = getDateAndTime();
 
-export const AI_NAME = "MyAI3";
-export const OWNER_NAME = "FirstName LastName";
+export const AI_NAME = "Alchemista";
+export const OWNER_NAME = "Rahil & Nishant";
 
 export const WELCOME_MESSAGE = `Hello! I'm ${AI_NAME}, an AI assistant created by ${OWNER_NAME}.`
 
